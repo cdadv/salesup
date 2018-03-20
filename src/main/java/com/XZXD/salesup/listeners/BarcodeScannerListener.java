050a3b497e1a617bd20339d75ca9ec0088ccd35f
Xuhui Ding <xuhui.ding@hotmail.com>
2018-03-19 22:41:32 -0700

    Barcode integration: concatenate scanned characters to a string.

--- src/main/java/com/XZXD/salesup/listeners/BarcodeScannerListener.java
+++ src/main/java/com/XZXD/salesup/listeners/BarcodeScannerListener.java
@@ -8,31 +8,49 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 @Service
 public class BarcodeScannerListener implements NativeKeyListener{
     private static final Logger logger = LoggerFactory.getLogger(BarcodeScannerListener.class);
+    // Empty the queue when detect enter
+    private static Queue<String> characterQueue = new LinkedList<>();
+    private static Queue<String> stringQueue = new LinkedList<>();
 
     @Override
     public void nativeKeyTyped(NativeKeyEvent nativeKeyEvent) {
-        logger.info("Key Typed: " + NativeKeyEvent.getKeyText(nativeKeyEvent.getKeyCode()));
+//        logger.info("Key Typed: " + NativeKeyEvent.getKeyText(nativeKeyEvent.getKeyCode()));
     }
 
     @Override
     public void nativeKeyPressed(NativeKeyEvent nativeKeyEvent) {
-        logger.info("Key Pressed: " + NativeKeyEvent.getKeyText(nativeKeyEvent.getKeyCode()));
-        logger.info(nativeKeyEvent.paramString());
+//        logger.info("Key Pressed: " + NativeKeyEvent.getKeyText(nativeKeyEvent.getKeyCode()));
+//        logger.info(nativeKeyEvent.paramString());
+        String currentCharacter = NativeKeyEvent.getKeyText(nativeKeyEvent.getKeyCode());
+        if (!currentCharacter.equals("Enter")) {
+            characterQueue.add(currentCharacter);
+        } else {
+            StringBuffer sb = new StringBuffer();
+            while (!characterQueue.isEmpty()) {
+                sb.append(characterQueue.remove());
+            }
+            stringQueue.add(sb.toString());
+            logger.info(sb.toString());
+        }
+        // Handle error
         if (nativeKeyEvent.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
             try {
                 GlobalScreen.unregisterNativeHook();
             } catch (NativeHookException e1) {
-                e1.printStackTrace();
+                logger.error(e1.getMessage());
             }
         }
     }
 
     @Override
     public void nativeKeyReleased(NativeKeyEvent nativeKeyEvent) {
-        logger.info("Key Released: " + NativeKeyEvent.getKeyText(nativeKeyEvent.getKeyCode()));
+//        logger.info("Key Released: " + NativeKeyEvent.getKeyText(nativeKeyEvent.getKeyCode()));
     }
 }
