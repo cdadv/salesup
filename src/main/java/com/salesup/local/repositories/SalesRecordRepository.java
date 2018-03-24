c40042b5ca16530bc6ed6a0268d2811f8b04c2ff
Xuhui Ding <xuhui.ding@hotmail.com>
2018-03-24 01:00:51 -0700

    Removed old 'src' module

--- src/main/java/com/salesup/local/repositories/SalesRecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.salesup.local.repositories;
-
-import com.salesup.local.domains.SalesRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SalesRecordRepository extends JpaRepository<SalesRecord, Long> {
-}
