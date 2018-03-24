965d0d0ea71c14614d541f62141be4b216122835
Xuhui Ding <xuhui.ding@hotmail.com>
2018-03-24 01:10:17 -0700

    'Develop' branch sync with 'Master' branch (#3)
    Removed old 'src' module

--- src/main/java/com/salesup/cloud/repositories/SalesRecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.salesup.cloud.repositories;
-
-import com.salesup.cloud.domains.SalesRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SalesRecordRepository extends JpaRepository<SalesRecord, Long> {
-}
